c3418fa8c2d518443f5d52de6d9f85136f0e4731
arithgrey <arithgrey@gmail.com>
2024-06-06 00:33:02 -0600

    [FIX] user on leads serializer

--- lead_search/serializers.py
+++ lead_search/serializers.py
@@ -1,11 +1,18 @@
 # serializers.py
 from rest_framework import serializers
 from lead.models import Lead
+import json
 
 
 class LeadSearchSerializer(serializers.ModelSerializer):
-    products = serializers.PrimaryKeyRelatedField(source='products_interest', many=True, read_only=True)
+    
+    products = serializers.SerializerMethodField() 
 
     class Meta:
         model = Lead
         fields = ['id', 'name', 'email', 'phone_number', 'status','created_at','products']
+    
+    def get_products(self, obj):
+        if obj.products_interest_ids:
+            return json.loads(obj.products_interest_ids)
+        return []
